8c2adfd8c8cd22e99c299d832fa77109aed15c3a
Anna Geller <anna@prefect.io>
2022-12-17 02:39:26 +0100

    ci/cd test

--- flows/hello.py
+++ flows/hello.py
@@ -5,8 +5,8 @@ from platform import node, platform
 @flow
 def hello(user_input: str = "World"):
     logger = get_run_logger()
+    logger.info("Hello from Prefect, %s! 🚀", user_input)
     logger.info("Network: %s. Instance: %s. Agent is healthy ✅️", node(), platform())
-    logger.info("Hello, %s! This confirms a new GH Action workflow run 🚀", user_input)
 
 
 if __name__ == "__main__":
