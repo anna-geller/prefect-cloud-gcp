16d9b29e57fd20951db4695d7c0eafd9f37b31ef
Anna Geller <anna@prefect.io>
2022-12-17 16:08:01 +0100

    fix image uri in block

---
+++ flows/marvin.py
@@ -0,0 +1,13 @@
+from prefect import flow, get_run_logger
+from platform import node, platform
+
+
+@flow
+def marvin(user_input: str = "Marvin"):
+    logger = get_run_logger()
+    logger.info("Hello from Prefect, %s! 🚀", user_input)
+    logger.info("Network: %s. Instance: %s. Agent is healthy ✅️", node(), platform())
+
+
+if __name__ == "__main__":
+    marvin()
